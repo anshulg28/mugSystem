d2763993345c10ce4770a59a352f0770d8d88409
anshul <anshulgupta@rocketmail.lcom>
2016-09-07 17:16:01 +0530

    event add page modified

--- views/mobile/ios/EventAddView.php
+++ views/mobile/ios/EventAddView.php
@@ -22,10 +22,24 @@
         <div class="page-content">
             <div class="content-block event-wrapper">
                 <div class="event-img-space">
-                    <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored event-img-add-btn">
-                        <i class="ic_add"></i>
-                    </button>
-                    <p class="add-img-caption">Add a cover photo</p>
+                    <div class="event-img-before">
+                        <input type="file" id="event-img-upload" onchange="uploadChange(this)" class="my-vanish"/>
+                        <!--<a href="#" class="button event-img-add-btn">
+                            <i class="ic_add"></i>
+                        </a>-->
+                        <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored event-img-add-btn">
+                            <i class="ic_add"></i>
+                        </button>
+                        <p class="add-img-caption">Add a cover photo</p>
+                    </div>
+                    <div class="event-img-after hide">
+                        <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored event-img-remove hide">
+                            <i class="ic_add"></i>
+                        </button>
+                        <div class="progress-bar">
+                            <div class="progressbar"></div>
+                        </div>
+                    </div>
                 </div>
                 <br>
                 <div class="event-descrip-wrapper">
@@ -52,7 +66,7 @@
                         <label class="mdl-textfield__label" for="eventDesc">Describe your event</label>
                     </div>
                     <select name="eventType" id="eventType" class="mdl-textfield__input">
-                        <option value="" selected>Event Type</option>
+                        <option value="" selected>Type of event</option>
                         <?php
                         foreach($this->config->item('eventTypes') as $key => $row)
                         {
@@ -62,6 +76,37 @@
                         }
                         ?>
                     </select>
+                    <br>
+                    <select id="eventPlace" name="eventPlace" class="mdl-textfield__input" required>
+                        <option value="">Location of event (max. 20 people)</option>
+                        <?php
+                        if(isset($locData))
+                        {
+                            foreach($locData as $key => $row)
+                            {
+                                if(isset($row['id']))
+                                {
+                                    ?>
+                                    <option value="<?php echo $row['id'];?>"><?php echo $row['locName'];?></option>
+                                    <?php
+                                }
+                            }
+                        }
+                        ?>
+                    </select>
+                    <br>
+                    <div class="row">
+                        <div class="col-50">
+                            <div class="input-group clockpicker" id="startTime">
+                                <input type="text" class="mdl-textfield__input" name="startTime" value="" placeholder="Start Time" readonly>
+                            </div>
+                        </div>
+                        <div class="col-50">
+                            <div class="input-group clockpicker" id="endTime">
+                                <input type="text" class="mdl-textfield__input" name="endTime" value="" placeholder="End Time" readonly>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="row">
